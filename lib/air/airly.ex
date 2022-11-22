d090d7342ac69ae64cba8e554ea6e17f5dbb5648
Marek Ciupak <6394528+marekciupak@users.noreply.github.com>
2022-11-22 22:41:17 +0100

    Delay fetching first data from Airly

--- lib/air/airly.ex
+++ lib/air/airly.ex
@@ -13,26 +13,23 @@ defmodule Air.Airly do
       lng: Application.fetch_env!(:air, :lng) |> String.to_float()
     }
 
-    measure(config)
+    schedule_measurement()
+
     {:ok, config}
-  end
-
-  @impl true
-  def handle_info(:measure, config) do
-    measure(config)
-
-    {:noreply, config}
   end
 
   defp schedule_measurement do
     Process.send_after(self(), :measure, :timer.minutes(15))
   end
 
-  defp measure(%{api_key: api_key, lat: lat, lng: lng}) do
+  @impl true
+  def handle_info(:measure, config = %{api_key: api_key, lat: lat, lng: lng}) do
     Airly.get_current_measurements(api_key: api_key, lat: lat, lng: lng)
     |> process_measurement()
 
     schedule_measurement()
+
+    {:noreply, config}
   end
 
   defp process_measurement(measurement) do
